5a4f3664766907a5664d3ddf3bc26518821bb7e3
Danilo Campos <mail@danilocampos.com>
2023-10-30 12:45:59 -0400

    Add type-safe bindings over valueData storage

--- Cogitator2/Model/Parameter.swift
+++ Cogitator2/Model/Parameter.swift
@@ -8,18 +8,14 @@
 import Foundation
 import CoreData
 import OpenAPIKit30
+import SwiftUI
 
 @objc(Parameter)
 public class Parameter: NSManagedObject, Comparable {
     public static func < (lhs: Parameter, rhs: Parameter) -> Bool {
         
-        if rhs.schema?.required == false, lhs.schema?.required == true {
-            return true
-        }
-        
         return lhs.sortIndex < rhs.sortIndex
     }
-    
     
     var schema: JSONSchema? {
         get {
@@ -29,5 +25,76 @@ public class Parameter: NSManagedObject, Comparable {
         set {
             self.schemaData = try? JSONEncoder().encode(newValue)
         }
+    }
+}
+
+//MARK - Bindings over underlying valueData storage
+extension Parameter {
+    var stringValue: Binding<String> {
+        Binding<String>(
+            get: {
+                if let data = self.valueData,
+                   let str = String(data: data, encoding: .utf8) {
+                    return str
+                }
+                return ""
+            },
+            set: {
+                if let data = $0.data(using: .utf8) {
+                    self.valueData = data
+                }
+            }
+        )
+    }
+
+    var intValue: Binding<Int> {
+        Binding<Int>(
+            get: {
+                if let data = self.valueData,
+                   let int = try? JSONDecoder().decode(Int.self, from: data) {
+                    return int
+                }
+                return 0
+            },
+            set: {
+                if let data = try? JSONEncoder().encode($0) {
+                    self.valueData = data
+                }
+            }
+        )
+    }
+
+    var floatValue: Binding<Float> {
+        Binding<Float>(
+            get: {
+                if let data = self.valueData,
+                   let float = try? JSONDecoder().decode(Float.self, from: data) {
+                    return float
+                }
+                return 0.0
+            },
+            set: {
+                if let data = try? JSONEncoder().encode($0) {
+                    self.valueData = data
+                }
+            }
+        )
+    }
+    
+    var boolValue: Binding<Bool> {
+        Binding<Bool>(
+            get: {
+                if let data = self.valueData,
+                   let bool = try? JSONDecoder().decode(Bool.self, from: data) {
+                    return bool
+                }
+                return false
+            },
+            set: {
+                if let data = try? JSONEncoder().encode($0) {
+                    self.valueData = data
+                }
+            }
+        )
     }
 }
